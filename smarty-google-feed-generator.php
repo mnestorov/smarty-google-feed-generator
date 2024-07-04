53fee63da70f727e9c3f82cfb2c19f55fb86a897
Martin Nestorov <mnestorov@users.noreply.github.com>
2024-07-05 01:27:56 +0300

    Added the new wp v6.5 option for Requires Plugins and remove unnecessary files

--- smarty-google-feed-generator.php
+++ smarty-google-feed-generator.php
@@ -23,7 +23,8 @@
  * Text Domain:             smarty-google-feed-generator
  * Domain Path:             /languages
  * WC requires at least:    3.5.0
- * WC tested up to:         5.1.0
+ * WC tested up to:         9.0.2
+ * Requires Plugins:		woocommerce
  */
 
 // If this file is called directly, abort.
